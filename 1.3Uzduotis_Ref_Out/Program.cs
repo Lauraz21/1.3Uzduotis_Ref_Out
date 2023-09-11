3ad9395e017e4bd68e6b72310f6afd5ba2db7694
Laura <Laura@DESKTOP-BESSB6E>
2023-09-11 22:01:00 +0300

    Add project files.

---
+++ 1.3Uzduotis_Ref_Out/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace _1._3Uzduotis_Ref_Out
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string text = "    labas   ";
+            TrimAndCapitalize(ref text);
+            Console.WriteLine(text);
+        }
+        public static void TrimAndCapitalize(ref string text)
+        {
+            text = text.Trim();
+            text = char.ToUpper(text[0]) + text.Substring(1);
+        }
+    }
+}
